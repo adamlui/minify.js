8dae308c901bef17c40592d583ba7094cd590467
adamlui <adamlui@users.noreply.github.com>
2024-04-13 08:40:26 +0000

    Loosened all comparison operators for smaller filesizes ↞ [auto-sync from `adamlui/js-utils`]

--- gulp/utils/build.sh
+++ gulp/utils/build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-minify-js src dist --comment=" \
-© 2024 Adam Lui under the MIT license.\n \
-Source: https://github.minify-js.org/tree/main/gulp/src\n \
-Documentation: https://docs.minify-js.org"
+minify-js src dist
